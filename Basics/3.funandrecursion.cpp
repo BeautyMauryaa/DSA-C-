0a75e4af8a03d58492e19298ff5c4eb976c0613e
Beauty Maurya <beautymaurya9142@gmail.com>
2026-04-19 00:50:39 +0530

    Function and recursion

--- Basics/3.funandrecursion.cpp
+++ Basics/3.funandrecursion.cpp
@@ -1,3 +1,113 @@
+//Notes: 
+//1.function: reusable block of code with name
+//syntax:
+//return_type function_name(parameters){
+//body
+//return value
+//}
+
+//2.pass byvalue and ref:
+//value->copy of original value,original safe
+//ref->address of original value ,original unsafe(original gets changed and & symbol)
+
+//use ref when:
+//we want to modify originla variable
+//when we want to return multiple values
+
+
+//3.return type:
+//void(return nothin), int(return integer), float(return decimal),bool(return true and false),string(return string), char(return characters),
+
+//cal only return one value directly
+//for multiple value-> use reference params
+
+//4.default paramters:
+//void greet(string name,string msg="hello")
+//rules: default parameters always on right side of non-default parameters
+//if argument passed->uses that
+//if not passed->uses default values
+
+
+//5.function overloading:
+//same function name but diff parameters
+//compiler picks correct one automatically
+
+//int add(int a,int b)
+//float add( float a,float b)
+//int add(int a,int b,int c)
+
+
+//6.scope and stack(imp):
+//local variables-> lives inside funciton only
+//global variable-> lives everywhere
+
+//stacke frame:
+//every function call= new frame create
+//frame has its own variables
+// function returns=frame destroyed
+
+
+
+//7.Recursion:
+//function calling itself directly or indirectly to solve a problem by breaking it down into smaller parts and solving each part recursively.
+//every recursion needs:
+//base case(when to stop)
+//recursive case(when to call again)
+
+
+//formula: 
+//whats the base case?
+//what's one step i can do?
+//trust recursion for the rest
+
+
+//recursin ex:
+int fact(int n){
+  if(n==0) return 1;//base case
+  return n*fact(n-1);//recursive case
+}
+
+void print(int n){
+  if(n==0) return;
+  print(n-1);//base case
+  cout<<n<<" ";
+}
+
+int fib(int n){
+  if(n<=1)return n;
+  return fib(n-1)+fib(n-2);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // function: reusable block of code that perform a specific task and can be called multiple times in a program.
 // recursion: functions that call themselves directly or indirectly to solve a problem by breaking it down into smaller parts and solving each part recursively.
 
@@ -33,6 +143,27 @@ void Addtenref(int &x)
   cout << "inside ref fun: " << x << endl;
 }
 
+//2. return type(define which type of value return back to the caller when caller will call this function or method)
+// and different type of parameters:()
+
+
+
+void str(string ask){//void return nothing
+  cout<<"How's everything going?"<<ask<<endl;
+}
+
+int sum(int a,int b){
+  return a+b;//return int type
+}
+
+//returning multiple values: use reference params(C++ functions return one value,but multiple values can be achieved using, references, pointers, or struct/pair.)
+void getvalue(int& a,int& b){
+  a=10;
+  b=10;
+}
+
+
+
 int main()
 {
   int x;
@@ -45,9 +176,26 @@ int main()
   Addtenref(x);
   cout << "after ref call: " << x << endl;
   cout << x << endl;
+
+
+  str("fine");
+  int ans=sum(3,4);
+  cout<<ans;
+
+
+  int a,b;
+  getvalue(a,b);
+  cout<<a<<" "<<b;
+
+
+  int c=8;
+  int factans=fact(c);
+  cout<<factans<<endl;
+
+  print(c);
+  int fibans=fib(c);
+  cout<<fibans;
   return 0;
 }
 
 
-//2. return type(define which type of value return back to the caller when caller will call this function or method)
-// and different type of parameters:()
